9627d72c72333824abf11dd9cb1fbc7ebb831b52
SkirnevskyiAleksandr <sanya-sha2016@ukr.net>
2022-05-18 20:08:28 +0300

    feat: 4-task

--- 4-lesson/app.js
+++ 4-lesson/app.js
@@ -1,21 +1,71 @@
 'use strict'
-function concatStrings(first) {
-    const f = first
-    return function (f) {
-        if (!isString(f)) {
-            return console.log("not a string");
+
+// //first task
+function concatStrings(first, separator) {
+    if (!isString(first)) {
+        return;
+    }
+
+    function nestedFunction(second) {
+        if (!isString(second)) {
+            return first;
         }
-        return console.log(f);
-    };
+
+        if (!isString(separator)) {
+            separator = '';
+        }
+
+        first += separator + second;
+
+        return nestedFunction;
+    }
+
+    return nestedFunction;
 }
 
 function isString(value) {
     return typeof value === 'string';
 }
 
-concatStrings()('sd');
 
+// second task
 
+class Calculator {
+    constructor(...theArgs) {
+        this.firstNumber = theArgs[0];
+        this.secondNumber = theArgs[1];
+        this.theArgs = theArgs;
+    }
+    isValue = () => {
+        return (this.theArgs.length === 2 && Number.isFinite(this.firstNumber)
+            && Number.isFinite(this.secondNumber));
+    }
+    setX = (num) => {
+        if (!Number.isFinite(num)) {
+            throw new Error(`you should enter valid number!`);
+        }
+        return this.firstNumber = num;
+    }
+    setY = (num) => {
+        if (!Number.isFinite(num)) {
+            throw new Error(`you should enter valid number!`);
+        }
+        return this.secondNumber = num;
+    }
+    logSum = () => {
+        console.log(this.firstNumber + this.secondNumber);
+    }
+    logMul = () => {
+        console.log(this.firstNumber * this.secondNumber);
+    }
+    logSub = () => {
+        console.log(this.firstNumber - this.secondNumber);
+    }
+    logDiv = () => {
+        if (!this.isValue() || this.secondNumber === 0) {
+            throw new Error(`please enter two valid numbers (second argument shouldn't be a zero)`);
+        }
+        console.log(this.firstNumber / this.secondNumber);
+    }
+}
 
-
-// concatStrings('first')('second')('third')().
